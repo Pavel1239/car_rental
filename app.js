89cd0815ffaf39f6d6474a435f4f082a810d9d9a
Pavel1239 <savonov9@gmail.com>
2018-10-03 10:52:14 +0300

    1

--- app.js
+++ app.js
@@ -24,7 +24,6 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/car', carRouter);
 
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
